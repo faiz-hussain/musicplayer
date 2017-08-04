7431c2ce79a42e65ecf06c88f2435a5894853f7a
Faizal Hussain <faizalhussain@Faizals-MacBook-Pro.local>
2017-08-04 16:16:56 -0400

    got buttons

--- src/components/SongsList.js
+++ src/components/SongsList.js
@@ -2,20 +2,14 @@ import React, {Component} from 'react';
 import {Link} from 'react-router';
 import '../index.css';
 //import 'bootstrap/dist/css/bootstrap.css';
-
-
 class SongsList extends Component {
     render() {
         let tunes = []
         tunes = this.props.songs.map((song, i)=>{
-            return (<Link to={"/songs/"+song.id}>{song.title}  </Link>
-            
-            
-            )
+            return <Link to={"/songs/"+song.id}>{song.title} <button onClick={()=> this.props.changeSong(i)}> <img className="play" src="./play.png"/> </button> </Link>
         })
         console.log(this.props.route)
         console.log(this.props.currentsong)
-
         return (
               <div className="container">
                 <div className="homebody" style={{'background-image':`url(${this.props.songs[this.props.currentsong].photo})`}}>
@@ -30,5 +24,4 @@ class SongsList extends Component {
         )
     }
 }
-
 export default SongsList;
